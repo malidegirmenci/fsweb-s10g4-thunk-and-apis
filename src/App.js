e9d7e4467fccba00594075de58339d4b75f67b40
malidegirmenci <m.alidegirmenci@hotmail.com>
2023-10-26 23:04:51 +0300

    created fetchHandler

--- src/App.js
+++ src/App.js
@@ -1,16 +1,24 @@
-import React from "react";
+import React, { useEffect } from "react";
 import { Switch, Route, NavLink } from "react-router-dom";
 import Item from "./components/Item";
 import FavItem from "./components/FavItem";
+import { useDispatch } from "react-redux";
+import { fetchAnother } from "./actions";
 
 export default function App() {
   const loading = false;
   const current = null;
   const favs = [];
-
+  const dispatch = useDispatch();
   function addToFavs() {
   }
 
+  const fetchHandler = () => {
+    dispatch(fetchAnother())
+  }
+  useEffect(() => {
+    fetchHandler();
+  }, [])
 
   return (
     <div className="wrapper max-w-xl mx-auto px-4">
