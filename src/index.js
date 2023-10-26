ea6a01331b7377890de94da7ace7ee69f427eaf4
malidegirmenci <m.alidegirmenci@hotmail.com>
2023-10-26 23:01:23 +0300

    imported thunk logger and added redux dev tools

--- src/index.js
+++ src/index.js
@@ -4,11 +4,14 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 import { BrowserRouter } from 'react-router-dom';
-import { legacy_createStore as createStore } from "redux";
+import { legacy_createStore as createStore, applyMiddleware } from "redux";
 import { Provider } from "react-redux";
 import { myReducer } from './reducers';
+import thunk from "redux-thunk";
+import logger from "redux-logger";
+import { composeWithDevTools } from '@redux-devtools/extension';
 
-const depo = createStore(myReducer);
+const depo = createStore(myReducer, composeWithDevTools(applyMiddleware(thunk,logger)));
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
