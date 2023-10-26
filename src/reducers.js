0b97ba4f64f1ca60a118f852acd859d54dc05494
malidegirmenci <m.alidegirmenci@hotmail.com>
2023-10-26 23:01:52 +0300

    handled fetching processes

--- src/reducers.js
+++ src/reducers.js
@@ -25,24 +25,24 @@ function readFavsFromLocalStorage() {
 export function myReducer(state = initial, action) {
   switch (action.type) {
     case FAV_ADD:
-      return state;
+      return {...state,};
 
     case FAV_REMOVE:
-      return state;
+      return {...state,};
 
     case FETCH_SUCCESS:
-      return state;
+      return {...state, loading:false, current:action.payload};
 
     case FETCH_LOADING:
-      return state;
+      return {...state, loading:true, error:null};
 
     case FETCH_ERROR:
-      return state;
+      return {...state, loading:false, error:action.payload};
 
     case GET_FAVS_FROM_LS:
-      return state;
+      return {...state,};
 
     default:
-      return state;
+      return {...state,};
   }
 }
