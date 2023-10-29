b18de9368c7cc5eec1088a15bea9e58e1f32d121
malidegirmenci <m.alidegirmenci@hotmail.com>
2023-10-29 16:31:38 +0300

    some css edited and added funcs

--- src/components/FavItem.js
+++ src/components/FavItem.js
@@ -1,14 +1,24 @@
 import React from "react";
+import { removeFav } from "../store/actions/userActions";
+import { useDispatch } from "react-redux";
 
-function FavItem({ title }) {
+function FavItem({ item }) {
+  console.log("item",item);
+  const dispatch = useDispatch();
+  const removeHandler = (id) => {
+    dispatch(removeFav(id));
+  }
   return (
-    <div className="bg-white shadow hover:shadow-lg p-3 pl-5 flex items-center group transition-all">
-      <div className="flex-1 pr-4">{title}</div>
+    <div className="bg-[#3b3b3b] shadow hover:shadow-lg p-3 pl-5 flex items-center group transition-all">
+      <div className="flex-1 flex justify-start items-center pr-4">
+        <img className='rounded' src={item.picture.large} alt={`ProfileImgBy${item.name.first}`} />
+        <p className="pl-4">{`${item.name.first} ${item.name.last} / ${item.favGame} / ${item.registered.age} years playing`}</p>
+      </div>
       <button
-        onClick={() => { }}
-        className="transition-all px-3 py-2 block text-sm rounded bg-rose-700 text-white opacity-30 group-hover:opacity-100"
+        onClick={() => {removeHandler(item.id.value)}}
+        className="transition-all px-3 py-2 block text-sm rounded bg-[#efefef] text-[#3b3b3b] opacity-30 group-hover:opacity-100"
       >
-        Çıkar
+        Remove
       </button>
     </div>
   );
